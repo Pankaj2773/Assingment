2a64da8ba5e2bb969cbff6fd77226de92c7c1d16
Pankaj Patil <patil.pankaj22494@gmail.com>
2018-06-23 14:41:54 +0530

    Assignment
    demo

---
+++ Assignment2/inline/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: ` 
+   <div style="text-align:center">
+  
+    <input type="text" name="fieldName" value="Marvellous Infosystem" readonly>
+     </div>  `,
+  styles: [`
+  {
+    background-color: #6d8eeb; 
+    width: 200px;
+     height: 3em;
+  }`]
+})
+export class AppComponent {
+  title = 'Inline2';
+}
